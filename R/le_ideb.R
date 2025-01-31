83747e409e2d624e29c217eae64e0f6fdcda961f
Rodrigo Emmanuel Santana Borges <rodrigo@borges.net.br>
2025-01-31 17:53:21 -0300

    added handling of error when attempting to download file taken from https://stackoverflow.com/questions/63340463/download-files-until-it-works

--- R/le_ideb.R
+++ R/le_ideb.R
@@ -13,7 +13,28 @@ le_ideb <- function(regiao="municipios",nivel="iniciais") {
 
   f <- tempfile()
 
-  download.file(caminho_fonte,f,method="curl")
+
+  retry <- \(expr, isError=function(x) "try-error" %in% class(x), maxErrors=5, sleep=0) {
+    attempts = 0
+    retval = try(eval(expr))
+    while (isError(retval)) {
+      attempts = attempts + 1
+      if (attempts >= maxErrors) {
+        msg = sprintf("retry: too many retries [[%s]]", capture.output(str(retval)))
+        stop(msg)
+      } else {
+        msg = sprintf("retry: error in attempt %i/%i [[%s]]", attempts,  maxErrors,
+                      capture.output(str(retval)))
+        warning(msg)
+
+      }
+      if (sleep > 0) Sys.sleep(sleep)
+      retval = try(eval(expr))
+    }
+    return(retval)
+  }
+
+  retry(download.file(caminho_fonte,f,method="curl"),maxErrors = 5,sleep = 1)
 
   availf <- unzip(f,list=T)
 
